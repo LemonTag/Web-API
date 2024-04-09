7e6ed092dd1ed6fe2907fd7d201a20a41e5462b6
Keegan Micu <LemonTag73@gmail.com>
2024-04-09 15:37:20 -0400

    fixed the alerts and made them into string that show in the console.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,10 +1,3 @@
-userNameInput = document.getElementById('username');
-titleNameInput = document.getElementById('title');
-contentNameInput = document.getElementById('content')
-themeSwitcher = document.getElementById('#theme-switcher');
-container = document.getElementById('container');
-showAlert= document.getElementById("submit");
-
 
 function myFunction() {
     var element = document.body;
@@ -12,25 +5,41 @@ function myFunction() {
 }
 
 function showAlert(event) {
-    const stringifyPost = localStorage.getItem('blogEntries')
-    const blogEnties = JSON.parse(stringPost) || [];
+// have a one piece of lego/ and then break back into one pieces/and you get your peices back
+    const stringPost = localStorage.getItem('blogEntries')
+    const blogEntries = JSON.parse(stringPost) || [];
     console.log("check");
-}
-const blogEntry = {
-    userNameEntry: userNameInput.value,
-    titleEntry: titleNameInput.value,
-    contentEntry: contentNameInput.value,
-}
-if (
-    blogEntry.userNameEntry == "" || blogEntry.titleEntry == "" || blogEntry.contentEntry == "") {
-    console.log(username.vlaue);
-    alert("fillin in all areas");
-    console.log("testing");
-}
 
-else {
-    blogEntries.push(blogEntry)
-    localStorage.setItem('blogEnties', JSON.stringify(blogEntries))
-    console.log("nextPage");
+    //you build an object with legos
+    //this the object, has three fields of the values of the form.
+    const blogEntry = {
+        userNameEntry: userNameInput.value,
+        titleEntry: titleNameInput.value,
+        contentEntry: contentNameInput.value,
+    }
+    if (
+        //
+        //scan the fields to see if any of them are empty
+        blogEntry.userNameEntry == '' || blogEntry.titleEntry == '' || blogEntry.contentEntry == '') {
+        console.log(username.vlaue);
+        alert("fillin in all areas");
+        console.log("testing");
+        event.preventDefault();
+    }
+
+    else {
+        //take your lego into an array into a string and put it in the local storage
+        //an Array to hold an object that gets put into this array
+        blogEntries.push(blogEntry)
+        //Making it into a string
+        localStorage.setItem('blogEnties', JSON.stringify(blogEntries))
+        console.log("nextPage");
+    }
 }
+submitPress = document.getElementById("submit");
+submitPress.addEventListener("click", showAlert);
+userNameInput = document.getElementById('username');
+titleNameInput = document.getElementById('title');
+contentNameInput = document.getElementById('content')
+
 
