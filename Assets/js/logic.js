b36e39daa7909395306fba610dd725b3fff5d63a
Keegan Micu <LemonTag73@gmail.com>
2024-04-08 23:53:44 -0400

    I have added a alerts for all my areas but they sumbit button can ignore that so I need find away to fix that, I have also made a go back button as well.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,40 +1,36 @@
-const userNameInput = document.querySelector('username');
-const titleNameInput = document.querySelector('title');
-const contentNameInput = document.querySelector('content')
-const themeSwitcher = document.querySelector('#theme-switcher');
-const container = document.querySelector('container');
+userNameInput = document.getElementById('username');
+titleNameInput = document.getElementById('title');
+contentNameInput = document.getElementById('content')
+themeSwitcher = document.getElementById('#theme-switcher');
+container = document.getElementById('container');
+showAlert= document.getElementById("submit");
 
 
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
+}
 
-const button = document.getElementById("clickable")
+function showAlert(event) {
+    const stringifyPost = localStorage.getItem('blogEntries')
+    const blogEnties = JSON.parse(stringPost) || [];
+    console.log("check");
+}
+const blogEntry = {
+    userNameEntry: userNameInput.value,
+    titleEntry: titleNameInput.value,
+    contentEntry: contentNameInput.value,
+}
+if (
+    blogEntry.userNameEntry == "" || blogEntry.titleEntry == "" || blogEntry.contentEntry == "") {
+    console.log(username.vlaue);
+    alert("fillin in all areas");
+    console.log("testing");
+}
 
+else {
+    blogEntries.push(blogEntry)
+    localStorage.setItem('blogEnties', JSON.stringify(blogEntries))
+    console.log("nextPage");
+}
 
-// sumbitButton.addEventListener('clickable', function (event) {
-//     event.preventDefault();
-
-//     setTimeout(function userNameInput() { alert("Username"); }, time);
-//     setTimeout(function titleNameInput() { alert("Tile"); }, time);
-//     setTimeout(function contentNameInput() { alert("Content"); }, time);
-//     // function myFunction() {
-    //     var element = document.body;
-    //     element.classList.toggle("dark-mode");
-    // }
-
-//     const user = {
-//         uName: userNameInput.value,
-//         title: titleNameInput.value,
-//         content: contentNameInput.value,
-//     }
-
-//     localStorage.setItem('userProfile', JSON.stringify(user));
-
-    
-
-//     location.href = "./blog.html";
-
-
-// });
